cb3f967e0da6a12b09184c11b7fee234d3a623f8
Debajyati <ddebajyati@gmail.com>
2025-05-02 17:02:40 +0530

    Update the profile screen with necessary changes according to @clerk/clerk-expo and firebase including a arrow-left button to go back prev screen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,16 +1,33 @@
 import ProfileScreen from "@/components/ProfileScreen";
-import React from "react";
+import type React from "react";
+import { useRouter } from "expo-router";
+import { Pressable } from "react-native";
+import ThemedText from "@/components/ThemedText";
+import { AntDesign } from "@expo/vector-icons";
+import styles from "@/app/styles";
 import { SafeAreaView } from "react-native-safe-area-context";
-import styles from "../styles";
 
-class Profile extends React.Component {
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <ProfileScreen />
-      </SafeAreaView>
-    );
-  }
+export default function Profile(): React.JSX.Element {
+  const router = useRouter();
+  return (
+    <SafeAreaView style={styles.container}>
+      <Pressable
+        onPress={() => router.back()}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          margin: 0,
+          padding: 0,
+          borderRadius: 100,
+          backgroundColor: "#ffffff",
+        }}
+      >
+        <ThemedText style={styles.buttonText}>
+          <AntDesign name="arrowleft" size={15} color="black" />
+        </ThemedText>
+      </Pressable>
+      <ProfileScreen />
+    </SafeAreaView>
+  );
 }
-
-export default Profile;
